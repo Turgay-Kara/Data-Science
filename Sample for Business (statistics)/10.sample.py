5ad8ca6aad85a2dc4a8beba4252570cc024313f7
Turgay KARA <84818047+Turgay-Kara@users.noreply.github.com>
2024-10-11 15:46:59 +0300

    Update 10.sample.py

--- Sample for Business (statistics)/10.sample.py
+++ Sample for Business (statistics)/10.sample.py
@@ -2,3 +2,9 @@
 IS UYGULAMASI
 
 """
+
+# Varyans Analizi: 2 veya dahya fazla grup ortalmasi arasinda istatiksel
+# olarak anlamli farklilik olup olmadigi ogrenilmek istenildiginde kullanilir.
+
+# H0 = Hipotezler birbirine Esittir.
+# H1 = En az 1 hipotez Farklidir.
