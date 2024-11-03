f9bd9ff68416809d61cb1a1701fdfd3a5a9a41be
Turgay KARA <84818047+Turgay-Kara@users.noreply.github.com>
2024-11-03 16:50:32 +0300

    Update 02.Python Programming.py

--- 02.Python Programming.py
+++ 02.Python Programming.py
@@ -1146,35 +1146,6 @@ print(alan1 + alan2)
 """
 
 
-    # BREAK
-#harfler = ["a", "b", "c", "d", "e"] * 1000
-#for index, harf in enumerate(harfler):
-    #if harf == "c":
-        #print("{} harfi {}. indexte!".format(harf, index))      #-> c harfinin kacinci indexte oldugunu soyler.
-        #break   #-> break komutu olmasaydi dongu 1000 kere tekrar edecekti ama istedigimizi elde ettigimiz anda cikmak istiyorsak "break" kullanabiliriz.
-
-
-    # CONTINUE
-#for sayi in range(1, 6):
-    #if sayi%2 == 0: #-> cift sayi sorgulama mantigi sayi%2 -> sayinin 2'ye bolumunden kalan 0 ise...
-        #continue    #-> continue oldugu icin islemi range'de verdigimiz son sayiya kadar devam ettirecek.
-    #print(sayi)     #-> tek olanlari yazdi cunku eger sayi 2'ye bolunuyorsa devam etmesini soyledik.
-
-
-    # PASS
-#for sayi in range(1, 6):
-    #if sayi%2 == 0:
-        #pass            #-> bu sekilde bir durum gorursen burayi atla.
-    #else:
-        #print(sayi)     #-> cift olma durumunu atladigimiz icin tek olanlari yazdirdik.
-
-
-#for sayi in range(1, 6):
-    #if sayi%2 != 0:     #-> Kalan 0'a esit degil ise...
-        #pass            #-> bu sekilde bir durum gorursen burayi atla. (Tek olan sayilari atla)
-    #else:
-        #print(sayi)     #-> Tek olanlari atladigimiz icin cift olanlari yazdirdik.
-
 
     # BREAK
 #harfler = ["a", "b", "c", "d", "e"] * 1000
